d3c32497cdb279c73aa2533f1adf75d073f0e91e
Jeevan Patel <jeevan.v.patel@gmail.com>
2024-10-19 00:16:48 +0530

    serless

--- src/main/java/com/mw/magicw/MagicwController.java
+++ src/main/java/com/mw/magicw/MagicwController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
-@CrossOrigin({"http://localhost:5173","https://admirable-smakager-729141.netlify.app/"})
+@CrossOrigin({"http://localhost:5173","https://admirable-smakager-729141.netlify.app/","http://localhost:8888"})
 
 //
 
@@ -20,19 +20,16 @@ public class MagicwController {
 
 	
 	static List<Blog> bs=List.of(new Blog(0, LocalDate.now(), 4, "", "मुनिगन निकट बिहग मृग जाहीं। बाधक बधिक बिलोकि पराहीं॥\r\n"
-			+ "\r\n"
-			+ "हित अनहित पसु पच्छिउ जाना। मानुष तनु गुन ग्यान निधाना॥\r\n"
+			+ "हित अनहित पसु पच्छिउ जाना। मानुष तनु गुन ग्यान निधाना॥"
 			+ "\r\n\n"
-			+ "पक्षी और पशु मुनियों के पास (बेधड़क) चले जाते हैं, पर हिंसा करने वाले बधिकों को देखते ही भाग जाते हैं। मित्र और शत्रु को पशु-पक्षी भी पहचानते हैं। फिर मनुष्य शरीर तो गुण और ज्ञान का भंडार ही है॥\r\n"
+			+ "पक्षी और पशु मुनियों के पास (बेधड़क) चले जाते हैं, पर हिंसा करने वाले बधिकों को देखते ही भाग जाते हैं। मित्र और शत्रु को पशु-पक्षी भी पहचानते हैं। फिर मनुष्य शरीर तो गुण और ज्ञान का भंडार ही है॥"
 			+ "\r\n\n"
 			+ "Birds and beasts go to the sages fearlessly, but they run away as soon as they see the violators. Even animals and birds recognize friends and foes. Then the human body is a storehouse of qualities and knowledge.\r\n"
 			+ "\r\n"
 			+ ""), new Blog(0, LocalDate.now(), 4, "", "बरषहिं जलद भूमि निअराएँ। जथा नवहिं बुध बिद्या पाएँ।\r\n"
-					+ "\r\n"
 					+ "बूँद अघात सहहिं गिरि कैसे। खल के बचन संत सह जैसें॥\r\n"
 					+ "\r\n"
 					+ "बादल पृथ्वी के समीप आकर (नीचे उतरकर) बरस रहे हैं, जैसे विद्या पाकर विद्वान्‌ नम्र हो जाते हैं। बूँदों की चोट पर्वत कैसे सहते हैं, जैसे दुष्टों के वचन संत सहते हैं॥\r\n"
-					+ "\r\n"
 					+ "\r\n"
 					+ "The clouds are coming close to the earth (descending) and it is raining, just as scholars become humble after gaining knowledge. How mountains bear the blow of drops, just as saints bear the words of the wicked."), 
 			new Blog(0, LocalDate.now(), 4, "", "धीरजु धरिअ त पाइअ पारू। नाहिं त बूड़िहि सबु परिवारू॥\n"
